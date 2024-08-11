8b99952bad95ff635be7037729f833cbca2eaaf4
al4an2 <al4an2@gmail.com>
2024-08-11 21:37:58 +0200

    Added DeleteFeedFollow for users

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/al4an2/goRssAggregator/internal/database"
+	"github.com/go-chi/chi"
 	"github.com/google/uuid"
 )
 
@@ -37,13 +38,37 @@ func (apiCfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.
 	respondWithJSON(w, 201, databaseFeedFollowToFeedFollow(feedFollow))
 }
 
-func (apiCfg *apiConfig) handlerGetFeedFollow(w http.ResponseWriter, r *http.Request, User database.User) {
+func (apiCfg *apiConfig) handlerGetFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
 
-	feedFollows, err := apiCfg.DB.GetFeedFollows(r.Context(), User.ID)
+	feedFollows, err := apiCfg.DB.GetFeedFollows(r.Context(), user.ID)
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Couldn't get feed follows: %s", err))
 		return
 	}
 
-	respondWithJSON(w, 201, databaseFeedFollowsToFeedFollows(feedFollows))
+	respondWithJSON(w, 200, databaseFeedFollowsToFeedFollows(feedFollows))
+}
+
+func (apiCfg *apiConfig) handlerDeleteFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+
+	feedFollowIDStr := chi.URLParam(r, "feedFollowID")
+	feedFollwID, err := uuid.Parse(feedFollowIDStr)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't parse feed follow id: %s", err))
+		return
+	}
+
+	err = apiCfg.DB.DeleteFeedFollows(r.Context(), database.DeleteFeedFollowsParams{
+		ID:     feedFollwID,
+		UserID: user.ID,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't delete feed follow: %s", err))
+		return
+	}
+
+	type feedFollowDeleted struct {
+		Unfollow string `json:"unfollow"`
+	}
+	respondWithJSON(w, 200, feedFollowDeleted{Unfollow: "succsess"})
 }
