0ccc7b54683babc2e5febb21b780c1233a293a5e
al4an2 <al4an2@gmail.com>
2024-08-11 22:40:32 +0200

    Added scraper for feeds with goroutines on test format(without adding in db)

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/al4an2/goRssAggregator/internal/database"
 	"github.com/go-chi/chi"
@@ -37,9 +38,12 @@ func main() {
 		log.Fatal("Can't connect to database:", err)
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
